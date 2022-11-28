5e66337b750ec68e66bfb8f6fb10734a8d933ab4
DanyaSht <danyloshtog@gmail.com>
2022-11-28 15:25:00 +0200

    add comment my first message

--- 1.c
+++ 1.c
@@ -3,10 +3,11 @@
 
 int main()
 {
-    double x = 0.357;
-    double y = 2.031;
-    double equation = pow(x,cos(y)) - pow(tan(x), sqrt(y-x)) - 19.12*log(pow(y,3));
-    printf("x = %.3lf\ny = %.3lf\n\n",x,y);
-    printf("Result of equation: %.3lf", equation);
-    return 0;
+	/*My first message*/
+	double x = 0.357;
+	double y = 2.031;
+	double equation = pow(x, cos(y)) - pow(tan(x), sqrt(y - x)) - 19.12 * log(pow(y, 3));
+	printf("x = %.3lf\ny = %.3lf\n\n", x, y);
+	printf("Result of equation: %.3lf", equation);
+	return 0;
 }
